1c92d5c23e4a8e1234f10091c1a1b293446b98fc
msmangechen <msmangechen@gmail.com>
2019-05-25 16:36:12 -0700

    add Header

---
+++ app/config/musiclist.js
@@ -0,0 +1,37 @@
+export const MUSIC_LIST = [
+	{
+			id: 1,
+			title: "Wave",
+			artist: "花たん",
+			file: "http://other.web.rh01.sycdn.kuwo.cn/resource/n1/32/24/4061173801.mp3",
+			cover: "http://hanekaoru.com/demo/project/musicplayer/images/7.jpg"
+		},
+		{
+			id: 2,
+			title: "心做し",
+			artist: "花たん",
+			file: "http://other.web.rg01.sycdn.kuwo.cn/resource/n2/47/80/1146169844.mp3",
+			cover: "http://hanekaoru.com/demo/project/musicplayer/images/2.jpg"
+		},
+		{
+			id: 3,
+			title: "からくりピエロ歌ってみた",
+			artist: "花たん",
+			file: "http://other.web.rg01.sycdn.kuwo.cn/resource/n3/9/67/1607238436.mp3",
+			cover: "http://hanekaoru.com/demo/project/musicplayer/images/3.jpg"
+		},
+		{
+			id: 4,
+			title: "Wake Up",
+			artist: "花たん",
+			file: "http://other.web.rh01.sycdn.kuwo.cn/resource/n3/32/39/3610642989.mp3",
+			cover: "http://hanekaoru.com/demo/project/musicplayer/images/4.jpg"
+		},
+		{
+			id: 5,
+			title: "黑猫",
+			artist: "花たん",
+			file: "http://other.web.rf01.sycdn.kuwo.cn/resource/n1/26/99/3523805226.mp3",
+			cover: "http://hanekaoru.com/demo/project/musicplayer/images/5.jpg"
+		}
+]
