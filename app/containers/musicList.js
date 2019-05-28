d23a42843acccc46d90936312c3cbd045689815c
msmangechen <msmangechen@gmail.com>
2019-05-28 16:11:38 -0700

    add musiclist

--- app/containers/musicList.js
+++ app/containers/musicList.js
@@ -1,1 +1,31 @@
 import React, { Component } from 'react';
+import MusicListItem from '../component/musicListItem';
+import './musicListItem.scss'
+
+// 整合 MusicListItem 后形成的 列表页组件（类似于 ul）
+class MusicList extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		// 循环渲染（需要加上 key 属性）
+		let listElements = this.props.musicList.map((item) => {
+			// focus 属性用于标注当前播放的是哪一首歌曲（return boolean）
+			return (
+				<MusicListItem focus={item === this.props.currentMusicItem} key={item.id} musicItem={item} />
+
+			);
+		});
+
+		return (
+			<div className='listBox'>
+				<ul>
+					{listElements}
+				</ul>
+			</div>
+		)
+	}
+}
+
+export default MusicList;
--- app/root.js
+++ app/root.js
@@ -1,2 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Header from './components/Header';
+import Player from './containers/player';
+import MusicList from './containers/musicList';
+import Pubsub from 'pubsub-js';
+
+// 音乐列表（需要添加大括号，default 导出则不需要）
+import { MUSIC_LIST } from './config/musiclist';
+
+class Root extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			musicList: MUSIC_LIST,
+			cuerrentMusicItem: MUSIC_LIST[0],
+			cycleModel: 'cycle'
+		}
+	}
+}
